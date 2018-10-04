d382eae36fc7ee141541b6d6c0bd94e32fb3135c
wildaparasti <wildasti98@gmail.com>
2018-10-04 20:11:21 +0700

    blm final

---
+++ src/main/java/com/apap/tutorial4/service/CarServiceImpl.java
@@ -0,0 +1,24 @@
+package com.apap.tutorial4.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.apap.tutorial4.model.CarModel;
+import com.apap.tutorial4.repository.CarDb;
+
+/**
+ * CarServiceImpl
+ */
+@Service
+@Transactional
+public class CarServiceImpl implements CarService {
+	@Autowired
+	private CarDb carDb;
+	
+	@Override
+	public void addCar(CarModel car) {
+		carDb.save(car);
+	}
+}
